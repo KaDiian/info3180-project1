33bebe9c44411e92a8d045603350a5c0b4c48cef
KaDiian <kadiidavidson@gmail.com>
2021-03-20 04:46:45 -0500

    test

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some$3cretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project1:password@localhost/project1'
 db = SQLAlchemy(app)
 app.config.from_object(Config)
 from app import views
